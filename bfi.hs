dd1b4bf6c6461bae6fb5d18bc90f6c32512e81ad
Yusuke Yasui <ysk.univ.1007@gmail.com>
2013-08-20 15:16:54 +0900

    refactored

--- bfi.hs
+++ bfi.hs
@@ -13,16 +13,12 @@ charToBFCommandTable = [('>', IncPtr),
 
 data BFCommand = IncPtr | DecPtr | IncDat | DecDat | OutDat | InpDat | JmpFwd | JmpBck deriving Eq
 
-instance Show BFCommand where
-  show x = [fromJust $ lookup x $ map (\(x, y) -> (y, x)) charToBFCommandTable]
-
-charToBFCommand :: Char -> BFCommand
 charToBFCommand ch = fromJust $ lookup ch charToBFCommandTable
-
 stringToBFCommands = map charToBFCommand
 
 data BFMemory = BFMemory { cells :: [Word8], ptr :: Word8 }
 
+initialBFMemory = BFMemory (replicate 256 (0 :: Word8)) (0 :: Word8)
 incBFMemoryPtr m = m { ptr = (succ $ ptr m) }
 decBFMemoryPtr m = m { ptr = (pred $ ptr m) }
 incBFMemoryDat m = setBFMemoryDat m $ succ $ getBFMemoryDat m
@@ -32,10 +28,7 @@ getBFMemoryDat (BFMemory cells ptr) = cells !! (fromIntegral ptr)
 
 data BFWorld = BFWorld { commands :: [BFCommand], commandPointer :: Int, memory :: BFMemory }
 
-instance Show BFWorld where
-  show (BFWorld cmds cptr (BFMemory cells ptr)) = "cmds = " ++ show cmds ++ ", cptr = " ++ show cptr ++ ", mcells = " ++ show cells ++ ", mptr = " ++ show ptr
-
-createInitialBFWorld prog = BFWorld (stringToBFCommands prog) 0 $ BFMemory (replicate 256 (0 :: Word8)) (0 :: Word8)
+createInitialBFWorld prog = BFWorld (stringToBFCommands prog) 0 initialBFMemory
 
 searchMatchingParen :: [BFCommand] -> Int -> Int
 searchMatchingParen cmds cptr = head [x | (Just x, Nothing) <- zip l (tail l)]
@@ -47,42 +40,58 @@ searchMatchingParen cmds cptr = head [x | (Just x, Nothing) <- zip l (tail l)]
                             JmpBck      -> Nothing
                             _           -> return $ p + 1) (return $ succ cptr)
 
-driveBFWorld :: BFWorld -> IO (Maybe BFWorld)
+driveBFWorld :: BFWorld -> Maybe (IO BFWorld)
 driveBFWorld world@(BFWorld cmds cptr mem) = if cptr >= length cmds then
-                                               return Nothing
+                                               Nothing
                                              else
-                                               io_mb_world'
+                                               Just io_world'
   where
-    io_mb_world'
+    io_world'
       | elem cmd [IncPtr, DecPtr, IncDat, DecDat] =
-        return $ Just $ world'_base { memory = (case cmd of IncPtr -> incBFMemoryPtr
-                                                            DecPtr -> decBFMemoryPtr
-                                                            IncDat -> incBFMemoryDat
-                                                            DecDat -> decBFMemoryDat) mem }
+        return $ world'_base { memory = (case cmd of IncPtr -> incBFMemoryPtr
+                                                     DecPtr -> decBFMemoryPtr
+                                                     IncDat -> incBFMemoryDat
+                                                     DecDat -> decBFMemoryDat) mem }
       | cmd == OutDat = do putChar $ chr $ fromIntegral $ getBFMemoryDat mem
-                           return $ Just world'_base
+                           return world'_base
       | cmd == InpDat = do ch <- getChar
-                           return $ Just $ world'_base { memory = setBFMemoryDat mem $ fromIntegral $ ord ch }
-      | cmd == JmpFwd = do (BFWorld _ _ mem') <- runSubWorld $ world { commands = drop (succ cptr) $ take (pred cptr') cmds
-                                                                     , commandPointer = 0 }
-                           return $ Just $ world'_base { memory = mem' }
+                           return $ world'_base { memory = setBFMemoryDat mem $ fromIntegral $ ord ch }
+      | cmd == JmpFwd = runSubWorld mem
       where
         cmd = (cmds !! cptr)
-        cptr' = succ $ case cmd of JmpFwd -> searchMatchingParen cmds cptr
-                                   _      -> cptr
-        world'_base = world { commandPointer = cptr' }
+        world'_base = world { commandPointer = (succ cptr) }
 
-        runSubWorld sw@(BFWorld _ _ smem) = if getBFMemoryDat smem == (0 :: Word8) then
-                                              return sw
-                                            else
-                                              do (BFWorld _ _ smem') <- run sw
-                                                 runSubWorld $ sw { memory = smem' }
+        runSubWorld :: BFMemory -> IO BFWorld
+        runSubWorld smem = if getBFMemoryDat smem == (0 :: Word8) then
+                             return world { commandPointer = cptr'
+                                          , memory = smem }
+                           else
+                             do (BFWorld _ _ smem') <- run $ initialSubWorld { memory = smem }
+                                runSubWorld smem'
+          where
+            cptr' = succ $ searchMatchingParen cmds cptr
+            initialSubWorld = world { commands = drop (succ cptr) $ take (pred cptr') cmds
+                                    , commandPointer = 0 }
 
 run :: BFWorld -> IO BFWorld
-run world = do mb_world' <- driveBFWorld world
-               case mb_world' of Nothing        -> return world
-                                 (Just world')  -> run world'
+run world = case driveBFWorld world of Nothing          -> return world
+                                       (Just io_world') -> io_world' >>= run
 
 bfi :: String -> IO ()
 bfi prog = do run $ createInitialBFWorld prog
               return ()
+
+instance Show BFCommand where
+  show x = [fromJust $ lookup x $ map (\(x, y) -> (y, x)) charToBFCommandTable]
+
+instance Show BFMemory where
+  show (BFMemory cells ptr) = "[" ++ foldl (\s (x, i) -> s ++ if i == ptr then
+                                                                "(" ++ show x ++ ")"
+                                                              else
+                                                                " " ++ show x ++ " ") "" (zip cells [0..]) ++ "]"
+
+instance Show BFWorld where
+  show (BFWorld cmds cptr mem) = "Commands:\n[" ++ foldl (\s (x, i) -> s ++ if i == cptr then
+                                                                              "(" ++ show x ++ ")"
+                                                                            else
+                                                                              " " ++ show x ++ " ") "" (zip cmds [0..]) ++ "]\nMemory:\n" ++ show mem
