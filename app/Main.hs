e32d428713f4655b0b6ffa7db076a14ba68cd81d
ysk_univ <ysk.univ.1007@gmail.com>
2021-03-13 12:38:11 +0900

    *** [app/Main.hs] update

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,29 @@
 module Main where
 
-import Lib
+import BFI.CommandRunner
+import BFI.Data.Command
+import BFI.ProgramLoader
+-- import Control.Monad.Except (MonadError (catchError))
+import Data.Semigroup ((<>))
+import Options.Applicative
 
 main :: IO ()
-main = someFunc
+main = runBFFile =<< execParser opts
+  where
+    opts =
+      info
+        ( argument str (metavar "FILE")
+            <**> helper
+        )
+        ( fullDesc
+            <> progDesc "Run FILE"
+            <> header "haskell-bfi - a BFI implemented in haskell"
+        )
+
+runBFFile :: FilePath -> IO ()
+runBFFile path = runProgram =<< readFile path
+
+runProgram :: String -> IO ()
+runProgram prog = do
+  runBFIState $ runCommands $ loadProgram prog
+  return ()
